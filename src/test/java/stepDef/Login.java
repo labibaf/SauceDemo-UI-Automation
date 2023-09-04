c458407ec8524b45efa8d7777c0ec442f3e8102b
LabibAF <labibfadhali12@gmail.com>
2023-09-04 14:02:09 +0700

    Delet unused import statement

--- src/test/java/stepDef/Login.java
+++ src/test/java/stepDef/Login.java
@@ -7,7 +7,6 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
